344972fdc72fdf27942c3d24d34393bfb17463a3
dino-21 <80396471+dino-21@users.noreply.github.com>
2021-07-21 10:56:16 +0900

    Add files via upload

---
+++ js/JsonEtc.js
@@ -0,0 +1,15 @@
+﻿            $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v2/search/image",
+        data: { query:"방탄소년단 CD"},
+        headers:{Authorization: "KakaoAK ec47a9bda05472ac2b77a05ffa4d70e8"}
+      })
+        .done(function( msg ) {
+
+            console.log( msg );
+	
+            $("#bangtan1").append("<img src='"+msg.documents[14].image_url+"'/>");
+            $("#bangtan2").append("<img src='"+msg.documents[62].image_url+"'/>");
+            $("#bangtan3").append("<img src='"+msg.documents[73].image_url+"'/>");
+
+        });
---
+++ js/JsonEtc2.js
@@ -0,0 +1,15 @@
+﻿            $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v2/search/image",
+        data: { query:"위대한쇼맨DVD"},
+        headers:{Authorization: "KakaoAK ec47a9bda05472ac2b77a05ffa4d70e8"}
+      })
+        .done(function( msg ) {
+
+            console.log( msg );
+	
+            $("#marble1").append("<img src='"+msg.documents[20].image_url+"'/>");
+            $("#marble2").append("<img src='"+msg.documents[55].image_url+"'/>");
+            $("#marble3").append("<img src='"+msg.documents[37].image_url+"'/>");
+
+        })
